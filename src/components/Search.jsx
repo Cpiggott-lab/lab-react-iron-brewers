48a97509e446463ac370b8534666cb0cb51c44f5
Cpiggott-lab <crpapc@gmail.com>
2025-05-13 15:05:04 +0200

    done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,31 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
 function Search() {
+  const [search, setSearch] = useState("");
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://ih-beers-api2.herokuapp.com/beers/search?q=" + search
+        );
+        setBeers(response.data);
+      } catch (error) {
+        console.error("Error fetching beers:", error);
+      }
+    };
+    if (search) {
+      fetchData();
+    } else {
+      setBeers([]);
+    }
+  }, [search]);
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
   return (
     <div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
       <div className="input-group mb-2 w-50">
@@ -10,6 +37,8 @@ function Search() {
         <input
           type="text"
           className="form-control search-bar"
+          value={search}
+          onChange={handleSearch}
         />
       </div>
     </div>
--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import beersJSON from "./../assets/beers.json";
-
+import axios from "axios";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function RandomBeersPage() {
   // Mock initial state, to be replaced by data from the Beers API. Store the beer info retrieved from the Beers API in this state variable.
@@ -10,14 +12,21 @@ function RandomBeersPage() {
   // React Router hook for navigation. We use it for the back button. You can leave this as it is.
   const navigate = useNavigate();
 
-
-  
   // TASKS:
   // 1. Set up an effect hook to make a request for a random beer from the Beers API.
   // 2. Use axios to make a HTTP request.
   // 3. Use the response data from the Beers API to update the state variable.
-
-
+  useEffect(() => {
+    axios
+      .get("https://ih-beers-api2.herokuapp.com/beers/random")
+      .then((response) => {
+        const beer = response.data;
+        setRandomBeer(beer);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   // The logic and the structure for the page showing the random beer. You can leave this as it is.
   return (
